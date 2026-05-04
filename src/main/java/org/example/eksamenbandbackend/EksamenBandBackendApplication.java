84013468736e9128367f2f913d48b07f79cf0462
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-05-04 09:40:31 +0200

    Initialize Spring Boot project with Maven, default configuration, and essential dependencies.

---
+++ src/main/java/org/example/eksamenbandbackend/EksamenBandBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamenbandbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenBandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksamenBandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamenbandbackend/EksamenBandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamenbandbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenBandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
