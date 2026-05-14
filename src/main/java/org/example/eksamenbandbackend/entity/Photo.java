2424cc45cdff42ceddacc652fa566e7c4c8672a5
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-05-14 10:30:43 +0200

    Add createdAt field and PrePersist method to Photo entity for timestamping

--- src/main/java/org/example/eksamenbandbackend/entity/Photo.java
+++ src/main/java/org/example/eksamenbandbackend/entity/Photo.java
@@ -1,13 +1,14 @@
 package org.example.eksamenbandbackend.entity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,11 @@ public class Photo {
     private LocalDate dateTaken;
 
     private String photographer;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
