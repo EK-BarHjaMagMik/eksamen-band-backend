5484567e727eec49b40344cec73308eac1a5191c
phaberen <magnusphaberen@gmail.com>
2026-05-18 15:20:21 +0200

    BandMemberResponse

---
+++ src/main/java/org/example/eksamenbandbackend/service/BandMemberService.java
@@ -0,0 +1,22 @@
+package org.example.eksamenbandbackend.service;
+
+import org.example.eksamenbandbackend.dto.BandMemberResponse;
+import org.example.eksamenbandbackend.repository.BandMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BandMemberService {
+
+    private final BandMemberRepository bandMemberRepository;
+
+    public BandMemberService(BandMemberRepository bandMemberRepository) {
+        this.bandMemberRepository = bandMemberRepository;
+    }
+
+    public List<BandMemberResponse>
+
+
+
+}
