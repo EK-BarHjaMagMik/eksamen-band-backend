0421f8e2401b1a73d556fb34ce2cdcd0dff0ab0e
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-05-21 14:10:19 +0200

    Increase length limit for `url` in `Photo` entity and add length constraint for `ticketLink` in `Show` entity

--- src/main/java/org/example/eksamenbandbackend/entity/Photo.java
+++ src/main/java/org/example/eksamenbandbackend/entity/Photo.java
@@ -30,7 +30,7 @@ public class Photo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 2048)
     private String url;
 
     private String caption;
