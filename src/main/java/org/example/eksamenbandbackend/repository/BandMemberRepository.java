df8729c20afc29935f0b0242b49412cde7c8f801
phaberen <magnusphaberen@gmail.com>
2026-05-25 12:49:57 +0200

    removed accidental unused import optional util

--- src/main/java/org/example/eksamenbandbackend/repository/BandMemberRepository.java
+++ src/main/java/org/example/eksamenbandbackend/repository/BandMemberRepository.java
@@ -4,7 +4,6 @@ import org.example.eksamenbandbackend.entity.BandMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BandMemberRepository extends JpaRepository<BandMember, Long> {
 
