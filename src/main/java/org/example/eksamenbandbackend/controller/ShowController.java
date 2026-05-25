7b2d338ac9cf73989b274384765ae4ad9af0394e
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-05-25 13:58:49 +0200

    Remove unused import for RequestParam in ShowController

--- src/main/java/org/example/eksamenbandbackend/controller/ShowController.java
+++ src/main/java/org/example/eksamenbandbackend/controller/ShowController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/shows")
