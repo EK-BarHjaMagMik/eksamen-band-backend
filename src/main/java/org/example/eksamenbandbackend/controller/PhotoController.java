e50d303e2e48619b4ccebb2e32597b404443c636
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-05-26 15:43:03 +0200

    Remove unused imports in PhotoController

--- src/main/java/org/example/eksamenbandbackend/controller/PhotoController.java
+++ src/main/java/org/example/eksamenbandbackend/controller/PhotoController.java
@@ -3,20 +3,15 @@ package org.example.eksamenbandbackend.controller;
 import java.util.List;
 
 import org.example.eksamenbandbackend.dto.PhotoResponse;
-import org.example.eksamenbandbackend.dto.UpdatePhotoRequest;
 import org.example.eksamenbandbackend.service.PhotoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/photos")
